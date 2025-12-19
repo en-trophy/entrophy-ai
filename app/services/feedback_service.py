e5f06dac993ca9f1e82d0c84a628e731c17f388d
p-junseo <belsono47@gmail.com>
2025-12-19 19:23:22 +0900

    feat : 프롬프트 개선

--- app/services/feedback_service.py
+++ app/services/feedback_service.py
@@ -15,8 +15,8 @@ def generate_feedback(lesson_id, user_feature, answer_feature, evaluation):
             정답 여부: {evaluation['is_correct']}
             점수: {evaluation['score']}
 
-            사용자에게 짧고 친절하게 영어로 피드백해 줘.
-            틀렸다면 어떤 부분을 고치면 좋은지 말해줘.
+         정답이 아니면 [정답 수어 특징]과 [사용자 수어 특징]을 비교해서 어떤 부분을 고쳐야 할지
+            사용자에게 짧고 친절하게 1-2문장 이내로 피드백해 줘.
             """
 
     return call_llm(prompt)
