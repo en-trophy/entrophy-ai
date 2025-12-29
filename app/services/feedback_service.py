f3a0381733858a1e6fc4ffb20cd6100ab72522a3
p-junseo <belsono47@gmail.com>
2025-12-30 02:16:25 +0900

    feat : 표정 반영 프롬프팅

--- app/services/feedback_service.py
+++ app/services/feedback_service.py
@@ -16,7 +16,7 @@ def generate_feedback(lesson_id, user_feature, answer_feature, evaluation):
             점수: {evaluation['score']}
 
         정답이면 짧게 축하만 해 주고, 틀렸으면 [정답 수어 특징]과 [사용자 수어 특징]을 비교해서 사용자가 왜 틀렸는지만
-            사용자에게 아주 짧고 핵심적으로 1 문장의 영어로 피드백해 줘. 강조 기호는 사용하지마. e.g.) Stretch your pinky finger, turn your wrist forward, etc..
+            사용자에게 아주 짧고 핵심적으로 1 문장의 영어로 피드백해 줘. 표정이 틀렸으면 그것도 같이 말해줘. 강조 기호는 사용하지마. e.g.) Stretch your pinky finger, turn your wrist forward, etc..
             """
 
     return call_llm(prompt)
