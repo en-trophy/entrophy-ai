0c840b1bcb8f96a17cfa2cc17ed21f130f9553a3
p-junseo <belsono47@gmail.com>
2025-12-31 22:35:06 +0900

    fix : 틀린 부분만 반영하도록 프롬프트 수정

--- app/services/feedback_service.py
+++ app/services/feedback_service.py
@@ -1,6 +1,6 @@
 from app.ai.llm_client import call_llm
 
-def generate_feedback(lesson_id, user_feature, answer_feature, evaluation):
+def generate_feedback(lesson_id, evaluation):
 
     # 정답이면 LLM 호출 안 함
     if evaluation["is_correct"]:
@@ -9,18 +9,12 @@ def generate_feedback(lesson_id, user_feature, answer_feature, evaluation):
     prompt = f"""
             너는 미국 수어 학습 코치야.
 
+            사용자가 수어 동작을 틀렸는데, 원래는 다음과 같이 동작해야 하는데 사용자가 그렇게 동작하지 않았어.
+
             [정답 수어 특징]
-            {answer_feature}
+            {evaluation["wrong_parts"]}
 
-            [사용자 수어 특징]
-            {user_feature}
-
-            [판정]
-            정답 여부: {evaluation['is_correct']}
-            점수: {evaluation['score']}
-
-        정답이면 짧게 축하만 해 주고, 틀렸으면 [정답 수어 특징]과 [사용자 수어 특징]을 비교해서 사용자가 왜 틀렸는지만
-            사용자에게 아주 짧고 핵심적으로 1 문장의 영어로 피드백해 줘. 표정이 틀렸으면 그것도 같이 말해줘. 강조 기호는 사용하지마. e.g.) Stretch your pinky finger, turn your wrist forward, etc..
+            사용자가 왜 틀렸는지, 그래서 어떻게 고쳐야 하는지 사용자에게 아주 짧고 핵심적으로 1 문장의 영어로 피드백해 줘. 지어야 하는 표정이 특성에 있다면 그것도 같이 말해줘. 강조 기호는 사용하지마. e.g.) Stretch your pinky finger, turn your wrist forward, etc..
             """
 
     return call_llm(prompt)
